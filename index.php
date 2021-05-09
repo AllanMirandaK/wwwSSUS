80f1317e31217260876e87a78a8ac65e2007ab60
Allan Miranda <allansmiranda3@gmail.com>
2021-05-09 12:19:18 -0300

    criacao do index
    adição do index. ainda falta ajustar alinhamento das opções que vai ter

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<title>Bem-vindo</title>
+
+	</head>
+	<body>
+		<div class="card-maior">
+			<div class="box">
+				<p class="titulo">ESCOLHA EM QUAL OPÇÃO VOCÊ SE ENCAIXA:</p>
+				<div class="opcao1">
+					
+				</div><!--
+				--><div class="opcao2">
+					
+				</div>
+			</div>
+			
+		</div>
+	</body>
+</html>
