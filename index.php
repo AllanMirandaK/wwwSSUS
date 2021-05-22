55a0c4f0bd6d0b3c302650cabea83b97f77f026e
jobimdana6@gmail.com <exemplo@seuemail.com.br>
2021-05-22 17:36:11 -0300

    configurando tela inicial e colocando tabela

--- index.php
+++ index.php
@@ -7,12 +7,19 @@
 	</head>
 	<body>
 		<div class="card-maior">
+			
 			<div class="box">
+			<p class="titulo">ESCOLHA EM QUAL OPÇÃO VOCÊ SE ENCAIXA:</p>
+				<p class="titulo">ESCOLHA EM QUAL OPÇÃO VOCÊ SE ENCAIXA:</p>
+
 				<p class="titulo">ESCOLHA EM QUAL OPÇÃO VOCÊ SE ENCAIXA:</p>
 				<div class="opcao1">
 					
 				</div><!--
 				--><div class="opcao2">
+
+					</div><!--
+				--><div class="opcao3">
 					
 				</div>
 			</div>
