63b92d5d5792d762682b1f59976f81865dd23ed2
jobimdana6@gmail.com <exemplo@seuemail.com.br>
2021-05-22 18:52:08 -0300

    finalizando tela inicial, faltando link

--- index.php
+++ index.php
@@ -4,26 +4,40 @@
 		<link rel="stylesheet" type="text/css" href="css/index.css">
 		<title>Bem-vindo</title>
 
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
 	</head>
 	<body>
 		<div class="card-maior">
 			
 			<div class="box">
-			<p class="titulo">ESCOLHA EM QUAL OPÇÃO VOCÊ SE ENCAIXA:</p>
-				<p class="titulo">ESCOLHA EM QUAL OPÇÃO VOCÊ SE ENCAIXA:</p>
+			<p class="log">SSUS</p>
+				<p class="duvidas">Dúvidas| Mais sobre nós | Contate-nos</p>
+				<button type="button" class="btn btn-outline-success login">Login</button>
 
-				<p class="titulo">ESCOLHA EM QUAL OPÇÃO VOCÊ SE ENCAIXA:</p>
+			<p class="titulo">SSUS</p>
+
+			<p class="subtitulo">Plataforma para consulta de processos médicos:</p>
+
+				<p class="subtitulo2">Escolha a opção que você se encaixa:</p>
+
 				<div class="opcao1">
 					
-				</div><!--
-				--><div class="opcao2">
+				</div>
+				<div class="opcao2">
+	
+				</div>
 
-					</div><!--
-				--><div class="opcao3">
+				<div class="opcao3">
 					
 				</div>
 			</div>
 			
 		</div>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	</body>
 </html>
