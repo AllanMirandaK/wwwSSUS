8c79a4807af4bbaba9df4494d0da8a1c7264c3e3
Allan Miranda <allansmiranda3@gmail.com>
2021-06-08 22:10:52 -0300

    menu agora é separado

---
+++ menu.php
@@ -0,0 +1,31 @@
+<nav id="sidebar">
+				<div class="p-4 pt-5">
+		  		<a href="#" class="img logo mb-5" style="background-image: url(images/logo.png);"></a>
+	        <ul class="list-unstyled components mb-5">
+	          <li class="active">
+	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Cadastros</a>
+	            <ul class="collapse list-unstyled" id="homeSubmenu">
+              <li>
+                    <a href="cad_paciente.php">Cadastrar Paciente</a>
+                </li>
+                <li>
+                    <a href="cad_medico.php">Cadastrar Médico</a>
+                </li>
+                <li>
+                    <a href="cad_consulta.php">Cadastrar Consulta</a>
+                </li>
+
+	            </ul>
+	          </li>
+	        
+            <li>
+                <a href="encaminhamento.php">Atendimento para encaminhamento</a>
+              </li>
+	        </ul>
+
+	        <div class="footer">
+	        
+	        </div>
+
+	      </div>
+    	</nav>
